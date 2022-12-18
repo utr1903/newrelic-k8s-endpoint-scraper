9a0c6bb79b35de4562963de9ea582c1ce0adfd2c
uturkarslan <ugurturkarslan@gmail.com>
2022-12-18 11:49:58 +0100

    Separate amd & arm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-helm template "scraper" \
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --arm)
+      arm="true"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+# ARM deployment
+if [[ $arm == "true" ]]; then
+  helm upgrade "scraper" \
+    --install \
+    --wait \
+    --debug \
+    --create-namespace \
+    --namespace "newrelic" \
+    --set image.repository="uturkarslan/newrelic-kubernetes-endpoint-scraper-arm" \
+    --set image.tag="1.0.0" \
+    "../charts/scraper"
+
+# AMD deployment
+else
+helm upgrade "scraper" \
+  --install \
+  --wait \
+  --debug \
   --create-namespace \
   --namespace "newrelic" \
+  --set image.repository="uturkarslan/newrelic-kubernetes-endpoint-scraper-amd" \
+  --set image.tag="1.0.0" \
   "../charts/scraper"
-
-# helm upgrade "scraper" \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace "test" \
-#   "../charts/scraper"
+fi
